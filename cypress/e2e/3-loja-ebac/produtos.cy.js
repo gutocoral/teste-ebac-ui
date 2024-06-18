cb6313bf29e829d01ce29de9a46ed90a4519de2b
Guto Coral <guto.coral@yahoo.com.br>
2024-06-18 08:44:50 -0300

    produtos e listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+   
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2) - Primeiro elemento será o "0"
+            .contains('Apollo Running Short')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+});
