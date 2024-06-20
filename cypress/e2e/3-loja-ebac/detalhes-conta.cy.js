373bd49788ca58551622fa7dfafc20aaf54b8717
Guto Coral <guto.coral@yahoo.com.br>
2024-06-20 07:03:50 -0300

    Mudanças aula 3

--- cypress/e2e/3-loja-ebac/detalhes-conta.cy.js
+++ cypress/e2e/3-loja-ebac/detalhes-conta.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-describe('Funcionalidade: Detalhes da conta', () => {
+describe('Funcionalidade: Detalhes da conta'), () => {
 
     beforeEach(() => {
         cy.visit('minha-conta/edit-account')
@@ -12,6 +12,5 @@ describe('Funcionalidade: Detalhes da conta', () => {
 it('Deve completar detalhes da conta com sucesso', () => {
     cy.detalhesConta('Guto', 'Coral', 'guto.qa')
     cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
-});
-    
-});
+})
+};
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
    
     beforeEach(() => {
-        cy.visit('produtos')
+       produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(2) - Primeiro elemento será o "0"
-            .contains('Apollo Running Short')
-            .click()
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
 
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Argus All-Weather Tank'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+    
+    it('Deve visitar a página do produtos', () => {
+        
+    });
+    
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
